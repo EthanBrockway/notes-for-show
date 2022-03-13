26c4bd92f185432db1fa07438a5527d262979219
Skronkie <ebrockway01@gmail.com>
2022-03-13 18:13:44 -0500

    added post request for notes.

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -3,22 +3,35 @@ const fs = require("fs");
 const router = require("express").Router();
 const { notes } = require("../db/db.json");
 
-function findById(id, notes) {
-  const result = notes.filter((notes) => notes.id === id)[0];
-  return result;
-}
-
 router.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "../db/db.json"));
 });
 
-router.get("/notes/:id", (req, res) => {
-  const result = findById(req.params.id, notes);
-  if (result) {
-    res.json(result);
+router.post("/notes", (req, res) => {
+  if (typeof req.body === "object") {
+    let db = fs.readFileSync(path.join(__dirname, "../db/db.json"));
+    db = JSON.parse(db);
+    let notePosted = false;
+    if (req.body.title) {
+      for (let i = 0; i < db.length; i++) {
+        if (db[i].title === req.body.title) {
+          notePosted = true;
+          break;
+        }
+      }
+    } else notePosted = true;
+    if (!notePosted) {
+      db.push(req.body);
+      fs.writeFileSync(
+        path.join(__dirname, "../db/db.json"),
+        JSON.stringify(db)
+      );
+    }
+    res.sendFile(path.join(__dirname, "../db/db.json"));
+  } else {
+    res.status(400);
+    res.end();
   }
-  console.log(notes);
-  res.send(404);
 });
 
 module.exports = router;
